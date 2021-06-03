22f49661fe19c20388282a96decec9ed6de55e9d
NGUYEN TIEN DUNG <59666831+dungnguyen-art@users.noreply.github.com>
2021-06-03 15:04:41 +0700

    Update bai5.js

--- bai5.js
+++ bai5.js
@@ -1,14 +1,15 @@
-let day;
+let day = prompt("Nhập ngày vào đê: ");
 console.log(day);
-let month;
+let month = prompt("Nhập tháng vào đê đê: ");
 console.log(month);
-let year;
+let year = prompt("Nhập năm vào cái nhể: ")
 console.log(year);
 
 let str = "";
-if(day >0 && day <= 30 &&
+// thang co 30 ngay
+if(day > 0 && day <= 30 &&
     (month == 4 || month == 6 || month == 9 || month == 11)){
-        str = "Hợp lệ";
+        str += "hople";
         console.log("Hợp lệ");
         if(day < 30)
             day++;
@@ -17,29 +18,30 @@ if(day >0 && day <= 30 &&
             month++;
         }
     }
-else if(month == 2 && day <= 28 && year % 400 ==0){
-        console.log("Hợp lệ");
-        str = "Hợp lệ";
-        if(day <28)
+
+else if(month == 2 && day <= 29 && (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0))){
+        console.log("Hợp lệ");//năm nhuận
+        str += "hople";
+        if(day < 28)
             day++;
         else{
             day = 1;
             month ++;
         }
 }
-else if(month == 2 && day <= 29){
+else if(month == 2 && day <= 28){
         console.log("Hợp lệ");
-        str = "Hợp lệ";
-        if(day < 29)
+        str += "hople";
+        if(day < 28)
             day ++;
         else {
             day = 1;
             month++;
         }
 }
-else if(day > 0 && day < 31 && month != 2 && month <= 12 && month >= 1){
+else if(day > 0 && day <= 31 && month != 2 && month <= 12 && month >= 1){
         console.log("Hợp lệ");
-        str = "Hợp lệ";
+        str += "hople";
         if(day < 31)
             day++;
         else if(month < 12){
@@ -54,10 +56,10 @@ else if(day > 0 && day < 31 && month != 2 && month <= 12 && month >= 1){
 }
 
 else{
-        str = "Không hợp lệ";
+        str = "khonghople";
         console.log("Không hợp lệ");
 }
 //5b
-if(str == "Hợp lệ"){
+if(str == "hople"){
     console.log(day + "/" + month + "/" + year);
 }
